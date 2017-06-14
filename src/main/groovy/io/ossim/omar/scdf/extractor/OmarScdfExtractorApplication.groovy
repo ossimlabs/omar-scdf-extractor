aa308bca886ceda1c9a871aca41c29017d9de900
Natalie <nroberts@radiantblue.com>
2017-06-14 18:30:52 -0400

    Added the library for stream.annotation.Output.

--- src/main/groovy/io/ossim/omar/scdf/extractor/OmarScdfExtractorApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/extractor/OmarScdfExtractorApplication.groovy
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
+import org.springframework.cloud.stream.annotation.Output
 import org.springframework.cloud.stream.messaging.Processor
 import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.SendTo
